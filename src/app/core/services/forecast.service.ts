defb2b28ca5f6964c80b79bbbf13bae5556b2c6c
sharifian-m <mzh.sharifian@gmail.com>
2022-01-31 11:49:33 +0330

    six commit

--- src/app/core/services/forecast.service.ts
+++ src/app/core/services/forecast.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { BehaviorSubject, Observable, EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { environment } from 'src/environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { WeatherInfo } from '../models/weather';
+
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -11,34 +14,40 @@ const httpOptions = {
 })
 export class ForecastService {
 
- 
+  apiUrl=environment.apiUrl;
+  apiKey=environment.apiKey;
+
   oneforecast: WeatherInfo = {
-    name:'--',
-    weather:[{main:'--', description: '--'}],
-   
+    name: '--',
+    weather: [{ main: '--', description: '--' }],
+
     main: {
-        feels_like: 0,
-        humidity: 0,
-        pressure: 0,
-        temp: 0,
-        temp_max: 0,
-        temp_min: 0,
+      feels_like: 0,
+      humidity: 0,
+      pressure: 0,
+      temp: 0,
+      temp_max: 0,
+      temp_min: 0,
+    }
   }
-  }
-    private  coordinate$ = new BehaviorSubject<WeatherInfo>(this.oneforecast);
-   currentcoordinate$ = this.coordinate$.asObservable();
+  private coordinate$ = new BehaviorSubject<WeatherInfo>(this.oneforecast);
+  currentcoordinate$ = this.coordinate$.asObservable();
 
-    
-  constructor(private http: HttpClient) {}
-  // return this.http.get(`${this.API_URL + 'contacts'}/${contactId}`) 
-  getWeather(location:any) {
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(location: any) {
     return this.http.get(
-     environment.apiUrl + location + '&units=metric' +'&appid=' + environment.apiKey
-    );
+      this.apiUrl + location + '&units=metric' + '&appid=' + this.apiKey)
+      .pipe(catchError(err => {
+        if (err.status === 404) {
+          console.log(`Location ${location} not found`);
+          return EMPTY
+        }
+      })
+      );
   }
-
-
-  setcoordinate (weatherinfo:WeatherInfo){
-this.coordinate$.next(weatherinfo);
+  setcoordinate(weatherinfo: WeatherInfo) {
+    this.coordinate$.next(weatherinfo);
   }
 }
